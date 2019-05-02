944bb6a1ce86f4c54bca7b57ba2667ddaa348eaa
王立心 <wanglixin@wanglixindeMacBook-Air-2.local>
2019-05-02 16:04:21 +0800

    hw1完成，了解JSON.parse()使用時機！

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -1,10 +1,10 @@
 const request = require('request');
 
-request('https://lidemy-book-store.herokuapp.com/books', function (error, response, body) {
+request('https://lidemy-book-store.herokuapp.com/books', function (body) {
     // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('statusCode:', response && response.statusCode);
     const data = JSON.parse(body);
-    for (i = 0; i <= 10; i++) {
+    for (let i = 0; i <= 10; i += 1) {
         console.log(data[i].id, data[i].name);
     }
 });
